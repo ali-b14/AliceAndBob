d806dfe7c853815f79af46659df3ca699d3d0af8
ali-b14 <alib714@gmail.com>
2024-03-04 19:42:32 -0500

    created scanner objectand input name section

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,25 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
-public class Main {
 
+
+import java.util.Scanner;
+
+public class Main {
+    Scanner s = new Scanner(System.in);
     public static void main(String[] args ){
-        // create a new object of class Main
+         // create a new object of class Main
 
         // call the compute() method on that new object
     }
 
     public void compute() {
         // Get input string of name from user
+        System.out.print("what is your first name?");
+        String name = s.nextLine();
         
         // Create conditional to check name against Alice and Bob
+        
 
         // Print greeting to screen if Alice or Bob are true
         
